ad1f63c0e78cf6c6b0e71c98eb42a5e1000c2ea8
AOTech <86697379+aotech7@users.noreply.github.com>
2023-02-20 22:12:22 -0800

    Add files via upload

---
+++ replit.py
@@ -0,0 +1,23 @@
+Last login: Mon Feb 20 20:58:26 on ttys002
+amakaozobia@Ijeamakas-MacBook-Pro ~ % code
+zsh: command not found: code
+amakaozobia@Ijeamakas-MacBook-Pro ~ % python3
+Python 3.11.0 (v3.11.0:deaf509e8f, Oct 24 2022, 14:43:23) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
